c01ec2b6b930aad5e7613064322e2fe38200abc9
Fabrice-sangwa <19sm476@esisalama.org>
2023-04-07 12:38:00 +0200

    Ajout d'une migration

--- src/blog/models.py
+++ src/blog/models.py
@@ -31,3 +31,27 @@ class BlogPost(models.Model):
             self.slug = slugify(self.title)
 
         super().save(*args, **kwargs)
+
+
+class Author(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=40)
+    wikipedia = models.URLField(blank=True, null=True)
+
+    @property
+    def autor_indetity(self):
+        return self.firstname, self.lastname
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=40)
+    price = models.FloatField(default=False, null=True)
+    summary = models.TextField(blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    category_list = ["Aventure", "Thriller", "Fantastique", "Romance", "Horreur", "Science-fiction"]
+    category = models.TextField(blank=True, null=True, default=category_list)
+    stock = models.IntegerField(default=0)
+
+    @property
+    def book_tile(self):
+        return self.title
